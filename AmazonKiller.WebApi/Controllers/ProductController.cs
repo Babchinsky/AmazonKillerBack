6c1d7c433094c61042857f36c01b176092727158
alexs <babchinsky.prog@gmail.com>
2025-05-19 17:32:07 +0300

    chore(postman): add Postman collections and environment for AmazonKiller API

--- AmazonKiller.WebApi/Controllers/ProductController.cs
+++ AmazonKiller.WebApi/Controllers/ProductController.cs
@@ -11,7 +11,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace AmazonKiller.WebApi.Controllers;
 
 [ApiController]
-[Route("api/[controller]")]
+[Route("api/products")]
 public class ProductController(IMediator mediator) : ControllerBase
 {
     /// <summary>
